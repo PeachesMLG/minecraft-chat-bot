dd47469889f4f1f675d78a3a56ad244c98991d8b
Peaches_MLG <PeachesMLG100@gmail.com>
2024-01-28 15:16:28 +0000

    auto reconnect

--- index.js
+++ index.js
@@ -9,20 +9,28 @@ if (!host || !email || !port || !password) {
   throw new Error('Please set the HOST, EMAIL, PORT, and PASSWORD environment variables before running the script.');
 }
 
-const bot = mineflayer.createBot({
-  host: host,
-  username: email,
-  auth: 'microsoft',
-  port: parseInt(port), // Parse port as an integer
-  version: '1.20.2',
-  password: password
-});
+function createBot(){
+  const bot = mineflayer.createBot({
+    host: host,
+    username: email,
+    auth: 'microsoft',
+    port: parseInt(port), // Parse port as an integer
+    version: '1.20.2',
+    password: password
+  });
 
 
-bot.on('chat', (username, message) => {
-  console.log(`${username}: ${message}`)
-})
+  bot.on('chat', (username, message) => {
+    console.log(`${username}: ${message}`)
+  })
 
-// Log errors and kick reasons:
-bot.on('kicked', console.log)
-bot.on('error', console.log)
+  bot.on('kicked', reason => {
+    console.log(`Kicked for: ${reason}`);
+    createBot();
+  });
+
+  bot.on('kicked', console.log)
+  bot.on('error', console.log)
+}
+
+createBot();
